db1212cfc98d05d7f454debe6efcfdeff6a6b1c0
William Verde <williamverde10@gmail.com>
2021-01-14 11:41:07 -0400

    +Added: crud fields on config

--- Config/config.php
+++ Config/config.php
@@ -2,5 +2,61 @@
 
 return [
     'name' => 'Icommerceagree',
-    'shippingName' => 'icommerceagree'
+    'shippingName' => 'icommerceagree',
+    /*
+    * Crud Fields To Frontend
+    */
+    'crudFields' => [
+        'title' => [
+            'value' => null,
+            'name' => 'title',
+            'type' => 'input',
+            'props' => [
+                'label' => 'icommerce::common.title'
+            ]
+        ],
+        'description' => [
+            'value' => null,
+            'name' => 'description',
+            'type' => 'input',
+            'props' => [
+                'label' => 'icommerce::common.description',
+                'type' => 'textarea',
+                'rows' => 3,
+            ]
+        ],
+        'status' => [
+            'value' => '0',
+            'name' => 'status',
+            'type' => 'select',
+            'props' => [
+              'label' => 'icommerce::common.status',
+              'useInput' => false,
+              'useChips' => false,
+              'multiple' => false,
+              'hideDropdownIcon' => true,
+              'newValueMode' => 'add-unique',
+              'options' => [
+                ['label' => 'Activo','value' => '1'],
+                ['label' => 'Inactivo','value' => '0'],
+              ]
+            ]
+        ],
+        'image' => [
+          'value' => (object)[],
+          'name' => 'mediasSingle',
+          'type' => 'media',
+          'props' => [
+            'label' => 'Image',
+            'zone' => 'image',
+            'entity' => "Modules\Icommerce\Entities\ShippingMethod",
+            'entityId' => null
+          ]
+        ],
+        'init' => [
+            'value' => 'Modules\Icommerceagree\Http\Controllers\Api\IcommerceAgreeApiController',
+            'name' => 'init',
+            'isFakeField' => true
+        ]
+    ],
 ];
